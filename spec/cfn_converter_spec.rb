b0d008a67262decbbd693e5e63ae457ecdf571e0
Nobuyuki Matsui <matsui.nobuyuki@tis.co.jp>
2014-09-10 06:32:28 +0000

    add scaffolded files

---
+++ spec/cfn_converter_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe CfnConverter do
+  it 'has a version number' do
+    expect(CfnConverter::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
